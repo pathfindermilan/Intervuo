3af4810b3ea6cc0e3c774c564dfb259dfdf2bad7
pathfinder.milan <pathfinder.milan@protonmail.com>
2024-10-08 02:42:21 +0200

    [ADD] Added docker compose for server

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+
+source /backend/.venv/bin/activate
+exec "$@"
+uv run manage.py migrate
+uv run manage.py runserver 0.0.0.0:8001 &
+
+uv run celery -A config worker --loglevel=info &
+
+wait -n
+
+exit $?
